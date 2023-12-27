d1d64793e4d80aa1091ae06a625d66ed1e63369a
TaeHoon <31303655+xoasml@users.noreply.github.com>
2023-12-27 13:42:21 +0900

    add : BrowserRouter

--- src/About.js
+++ src/About.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function About() {
     return (
-        <div></div>
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트입니다.</p>
+        </div>
     );
 }
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes, Link} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+            </ul>
+            <hr/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
